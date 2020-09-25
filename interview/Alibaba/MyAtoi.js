f89db09ab2d9ad57599c754e2ca6a96527a9132d
yangdengxian <yangdengxian@cnpc.com.cn>
2020-09-25 16:01:17 +0800

    字符串转数字试题

--- interview/Alibaba/MyAtoi.js
+++ interview/Alibaba/MyAtoi.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var myAtoi = function(str) {
+    var num = 0;
+    num = parseInt(str);
 
+    if (isNaN(num)) {
+        return 0
+    }
+
+    return num > Math.pow(2, 31) - 1 ? Math.pow(2, 31) - 1 : (
+        Math.pow(-2, 31) > num ? Math.pow(-2, 31) : num
+    )
 };
+
+var num = myAtoi("words and 987")
